812f9a9d4440553180a8091dddec0f273c5729f0
dontkillmyvibee <lysorryoops@gmail.com>
2026-04-10 20:03:12 +0300

    Практическое задание: конфигурация Postgres для тестового окружения

--- tests/config.py
+++ tests/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from tests.tools.config.grpc import GRPCClientTestConfig, GRPCServerTestConfig
 from tests.tools.config.http import HTTPClientTestConfig, HTTPServerTestConfig
 from tests.tools.config.kafka import KafkaClientTestConfig
+from tests.tools.config.postgres import PostgresClientTestConfig
 
 
 class TestSettings(BaseSettings):
@@ -31,6 +32,8 @@ class TestSettings(BaseSettings):
 
     mock_http_server: HTTPServerTestConfig
     mock_grpc_server: GRPCServerTestConfig
+
+    operations_postgres_client: PostgresClientTestConfig
 
     """
     Таймаут ожидания асинхронной обработки операций.
